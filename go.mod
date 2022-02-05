d79fbeea98af35de196a44391705910ca22c7f28
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-05 22:24:43 +0700

    update func with param

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/sumitroajiprabowo/go-say-thankyou
+module github.com/sumitroajiprabowo/go-say-thankyou/v2
 
 go 1.17
--- say_thankyou.go
+++ say_thankyou.go
@@ -1,5 +1,5 @@
 package thankyou
 
-func SayThankYou() string{
-	return "Thankyou Go"
+func SayThankYou(name string) string {
+	return "Thankyou" + name
 }
